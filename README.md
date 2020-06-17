bcf4e44560074fbbfe54827e3ebe4c22682145bc
Hasnain Hossain <hasnainhossain@yahoo.com>
2020-06-17 15:42:06 +0600

    download script complete

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Getting Started with Web Scraping
 
-Will be updating this as I go
+Working with `beautifulsoup` and `requests`
+
+Parser: `html5lib`(`lxml` does not work for some reason)
+
+## Files
+
+- `alon_colon.py` - Scraping dataset for gene microarray expression, which is in `html`
+- `springer-fetching-pdfs.py` - Automating downloading free Springer books
+
+## Helpful guidelines found here:
+
+- [Downloading with urllib](https://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python)
+- [Using Unicode Normalization to Represent Strings](https://docs.microsoft.com/en-us/windows/win32/intl/using-unicode-normalization-to-represent-strings)
+- [Unicode HOWTO](https://docs.python.org/3/howto/unicode.html)
+- [Unicode Equivalence](https://en.wikipedia.org/wiki/Unicode_equivalence)
+- [unicodedata.normalize](https://docs.python.org/2/library/unicodedata.html#unicodedata.normalize)
--- springer-fetching-pdfs.py
+++ springer-fetching-pdfs.py
@@ -11,44 +11,42 @@ raw = requests.get(source).text
 
 soup = BeautifulSoup(raw, 'html5lib')
 
-csv_file = open('springer_links.csv', 'w')
+csv_file = open('springer-books-info.csv', 'w')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['link'])
+csv_writer.writerow(['Title', 'Author', 'Link'])
 
+error_list = []
 
-# # # 
-# tag = soup.find('a', class_='cq gv ie if ig ih')
-# redirect = requests.get(f'{tag.text}').text
-# parsed_url = urlparse(tag.text)
+for tag in tqdm(soup.find_all('a', class_='cq gv ie if ig ih')):
+    redirect = requests.get(f'{tag.text}').text
+    parsed_url = urlparse(tag.text)
 
-# spr_soup = BeautifulSoup(redirect, 'html5lib')
+    spr_soup = BeautifulSoup(redirect, 'html5lib')
 
-# dl_path = spr_soup.find('a', class_='c-button c-button--blue c-button__icon-right test-download-book-options test-bookpdf-link')['href']
-# link = '{}://{}{}'.format(parsed_url.scheme, parsed_url.hostname, dl_path)
+    # Retrieving title and author names:
+    content = spr_soup.find('div', class_='evaluation-section__text-col')
+    name = content.find('div', class_='page-title').h1.text
+    authorlist = [author.text.replace(u'\xa0', u' ') for author in \
+                  content.find_all('span', class_='authors__name')]
+    authorlist = ', '.join(authorlist)
 
-# # Using urllib:
-# # urlretrieve(link, '1.pdf')
+    csv_writer.writerow([name, authorlist, tag.text])
 
-# # Using requests:
-# response_obj = requests.get(link, stream=True)
-# with open('test.pdf', 'wb') as f:
-#     # The following is preferred, although you can simply write instead
-#     for chunk in tqdm(response_obj.iter_content(chunk_size=1024)):
-#         if chunk:
-#             f.write(chunk)
+    try:
+        button = spr_soup.find('div', class_='cta-button-container__item')
+        link = '{}://{}{}'.format(parsed_url.scheme, parsed_url.hostname, button.a['href'])
 
+        urlretrieve(link, f'{name} - {authorlist}.pdf')
+    except Exception as e:
+        error_list = error_list.append((name, link))
 
-# for tag in soup.find_all('a', class_='cq gv ie if ig ih'):
-#     redirect = requests.get(f'{tag.text}').text
-#     parsed_url = urlparse(tag.text)
+    # Alternatively, using requests for larger streams
+    # response_obj = requests.get(link, stream=True)
+    # with open('test.pdf', 'wb') as f:
+    #     # The following is preferred, although you can simply write instead
+    #     for chunk in tqdm(response_obj.iter_content(chunk_size=1024)):
+    #         if chunk:
+    #             f.write(chunk)
 
-#     spr_soup = BeautifulSoup(redirect, 'html5lib')
-
-#     dl_path = spr_soup.find('a', class_='c-button c-button--blue c-button__icon-right test-download-book-options test-bookpdf-link')['href']
-#     link = '{}://{}{}'.format(parsed_url.scheme, parsed_url.hostname, dl_path)
-
-#     urlretrieve(link, '1.pdf')
-    # csv_writer.writerow([tag.text])
-    # print(tag.text)
-
+print(error_list)
 csv_file.close()
